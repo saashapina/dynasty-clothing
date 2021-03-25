25218568eacfb526e575297be440959c31350aca
saashapina <saasha.pina@gmail.com>
2021-03-25 17:46:03 -0500

    created project with CRAand added home page

---
+++ src/pages/Home/home.js
@@ -0,0 +1,54 @@
+import "./home.scss";
+
+export const Home = () => {
+  return (
+    <div className="Home">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content-container">
+            <div className="content">
+              <h1>Hats</h1>
+              <div>SHOP NOW</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content-container">
+            <div className="content">
+              <h1>Jackets</h1>
+              <div>SHOP NOW</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content-container">
+            <div className="content">
+              <h1>Womens</h1>
+              <div>SHOP NOW</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content-container">
+            <div className="content">
+              <h1>Mens</h1>
+              <div>SHOP NOW</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content-container">
+            <div className="content">
+              <h1>Hats</h1>
+              <div>SHOP NOW</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
