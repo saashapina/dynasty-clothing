0643c9b780e1f6581c44a3d837367a974223ad6d
saashapina <saasha.pina@gmail.com>
2021-06-13 11:49:56 -0400

    clean up redux store logging for prod

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,10 +10,6 @@ if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
-if (process.env.NODE_ENV === "production") {
-  console.log("this is prod");
-}
-
 export const store = createStore(
   persistedRootReducer,
   applyMiddleware(...middlewares)
